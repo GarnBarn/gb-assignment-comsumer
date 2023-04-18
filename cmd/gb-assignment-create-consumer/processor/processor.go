7d526f3aef39d07f167a0394a374be6087c4c5c3
Siratee K <sirateek@gmail.com>
2023-04-18 23:06:59 +0700

    feat: Implement the create consumer

---
+++ cmd/gb-assignment-create-consumer/processor/processor.go
@@ -0,0 +1,46 @@
+package processor
+
+import (
+	"encoding/json"
+
+	globalmodel "github.com/GarnBarn/common-go/model"
+	rabbitMQ "github.com/GarnBarn/common-go/rabbitmq"
+	"github.com/GarnBarn/gb-assignment-consumer/pkg/repository"
+	"github.com/sirupsen/logrus"
+	"github.com/wagslane/go-rabbitmq"
+)
+
+type Processor struct {
+	rabbitmqPublisher    *rabbitmq.Publisher
+	assignmentRepository repository.AssignmentRepository
+}
+
+func NewProcessor(rabbitmqPublisher *rabbitmq.Publisher, assignmentRepository repository.AssignmentRepository) rabbitMQ.Processor {
+	return Processor{
+		rabbitmqPublisher:    rabbitmqPublisher,
+		assignmentRepository: assignmentRepository,
+	}
+}
+
+const (
+	RountingKeyCreate = "create"
+	RoutingKeyDelete  = "delete"
+)
+
+func (p Processor) Process(d rabbitmq.Delivery) error {
+	assignment := globalmodel.Assignment{}
+	err := json.Unmarshal(d.Body, &assignment)
+	if err != nil {
+		logrus.Error("Can't unmarshal data: ", err)
+		return err
+	}
+
+	err = p.assignmentRepository.CreateAssignment(&assignment)
+	if err != nil {
+		logrus.Error("Can't save data: ", err)
+		return err
+	}
+
+	logrus.Info("Successfully created the assignment id: ", assignment.ID)
+	return nil
+}
---
+++ pkg/repository/repository.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"github.com/GarnBarn/common-go/model"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type AssignmentRepository interface {
+	CreateAssignment(assignment *model.Assignment) error
+}
+
+type assignmentRepository struct {
+	db *gorm.DB
+}
+
+func NewAssignmentRepository(db *gorm.DB) AssignmentRepository {
+	// Migrate the db
+	db.AutoMigrate(&model.Assignment{})
+
+	return &assignmentRepository{
+		db: db,
+	}
+}
+
+func (a *assignmentRepository) CreateAssignment(assignmentData *model.Assignment) error {
+	logrus.Debug("Executing Create on %T", assignmentData)
+
+	res := a.db.Create(assignmentData)
+
+	// HandleError
+	if res.Error != nil && res.Error != gorm.ErrRecordNotFound {
+		logrus.Error(res.Error)
+		return res.Error
+	}
+
+	a.db.First(assignmentData, assignmentData.ID)
+	return nil
+}
